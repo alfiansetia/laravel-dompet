47c55a880762b97243d7c3e81808af352588edc2
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2024-03-17 10:17:53 +0700

    hide menu

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -43,29 +43,31 @@
                     </div>
                 </a>
             </li>
-            <li class="menu">
-                <a href="#master_menu" data-toggle="collapse"
-                    data-active="{{ $title == 'Data User' || $title == 'Data Dompet' ? 'true' : 'false' }}"
-                    aria-expanded="{{ $title == 'Data User' || $title == 'Data Dompet' ? 'true' : 'false' }}"
-                    class="dropdown-toggle">
-                    <div class="">
-                        <i data-feather="hard-drive"></i>
-                        <span>Master Data</span>
-                    </div>
-                    <div>
-                        <i data-feather="chevron-right"></i>
-                    </div>
-                </a>
-                <ul class="collapse submenu list-unstyled {{ $title == 'Data User' || $title == 'Data Dompet' ? 'show' : '' }}"
-                    id="master_menu" data-parent="#accordionExample">
-                    <li class="{{ $title == 'Data Dompet' ? 'active' : '' }}">
-                        <a href="{{ route('dompet.index') }}">Dompet</a>
-                    </li>
-                    <li class="{{ $title == 'Data User' ? 'active' : '' }}">
-                        <a href="{{ route('user.index') }}">User</a>
-                    </li>
-                </ul>
-            </li>
+            @if ($user->role == 'admin')
+                <li class="menu">
+                    <a href="#master_menu" data-toggle="collapse"
+                        data-active="{{ $title == 'Data User' || $title == 'Data Dompet' ? 'true' : 'false' }}"
+                        aria-expanded="{{ $title == 'Data User' || $title == 'Data Dompet' ? 'true' : 'false' }}"
+                        class="dropdown-toggle">
+                        <div class="">
+                            <i data-feather="hard-drive"></i>
+                            <span>Master Data</span>
+                        </div>
+                        <div>
+                            <i data-feather="chevron-right"></i>
+                        </div>
+                    </a>
+                    <ul class="collapse submenu list-unstyled {{ $title == 'Data User' || $title == 'Data Dompet' ? 'show' : '' }}"
+                        id="master_menu" data-parent="#accordionExample">
+                        <li class="{{ $title == 'Data Dompet' ? 'active' : '' }}">
+                            <a href="{{ route('dompet.index') }}">Dompet</a>
+                        </li>
+                        <li class="{{ $title == 'Data User' ? 'active' : '' }}">
+                            <a href="{{ route('user.index') }}">User</a>
+                        </li>
+                    </ul>
+                </li>
+            @endif
 
             <li class="menu">
                 <a href="{{ route('report.index') }}" data-active="{{ $title == 'Report' ? 'true' : 'false' }}"
@@ -95,12 +97,14 @@
                     <li class="{{ $title == 'User Profile' ? 'active' : '' }}">
                         <a href="{{ route('user.profile') }}">Profile</a>
                     </li>
-                    <li class="{{ $title == 'Company Setting' ? 'active' : '' }}">
-                        <a href="{{ route('comp.index') }}">Company</a>
-                    </li>
-                    <li class="{{ $title == 'Database Backup' ? 'active' : '' }}">
-                        <a href="{{ route('database.index') }}">Database Backup</a>
-                    </li>
+                    @if ($user->role == 'admin')
+                        <li class="{{ $title == 'Company Setting' ? 'active' : '' }}">
+                            <a href="{{ route('comp.index') }}">Company</a>
+                        </li>
+                        <li class="{{ $title == 'Database Backup' ? 'active' : '' }}">
+                            <a href="{{ route('database.index') }}">Database Backup</a>
+                        </li>
+                    @endif
                 </ul>
             </li>
         </ul>
