bf9852301ba36bba11a61cb8fe2e532db1bd5f0c
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2024-06-13 14:07:28 +0700

    Update CapitalController.php

--- app/Http/Controllers/CapitalController.php
+++ app/Http/Controllers/CapitalController.php
@@ -2,38 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Capital;
-use App\Models\Comp;
-use App\Models\Dompet;
 use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\DB;
-use Yajra\DataTables\Facades\DataTables;
 
 class CapitalController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('admin')->only(['destroy']);
+        $this->middleware('admin');
         $this->middleware('active');
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request)
+    public function index()
     {
-        if ($request->ajax()) {
-            $user = auth()->user();
-            $data = Capital::query();
-            if ($user->role == 'user') {
-                $data->where('user_id', $user->id);
-            }
-            $result = $data->with('dompet', 'user');
-            return DataTables::of($result)->toJson();
-        }
         return view('capital.index');
     }
 }
